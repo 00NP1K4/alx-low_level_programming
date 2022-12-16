038d82ecdc5b25c53c0d1291191fa5b2f8c3cc2b
00np1k4 <adeshegz32@gmail.com>
2022-12-15 17:18:30 -0800

    pusii

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,9 +6,9 @@
 #include"main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * return: 0
+ */
 
 int main(void)
 {
