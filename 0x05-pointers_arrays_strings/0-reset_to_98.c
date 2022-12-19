b95b24f5810428df037fbbfe3d5ea673f572b38e
00np1k4 <adeshegz32@gmail.com>
2022-12-19 08:39:57 -0800

    01

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/**
+ * File: 0-reset_to_98.c
+ * Author: Noah Adeshina
+ */
+#include "main.h"
+/**
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+new 0x05-pointers_arrays_strings
